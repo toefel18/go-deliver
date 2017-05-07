f6bdf12b598586d9c965a18a77c637e695d46905
christophe.o.hesters <toefel18@gmail.com>
2017-05-07 20:55:35 +0200

    backend run script now runs in background

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 go build main.go
-sudo PORT=443 APP_SOURCES=/home/ubuntu/src/github.com/toefel18/go-deliver/DHLapp/build/default ./main
+sudo PORT=443 APP_SOURCES=/home/ubuntu/src/github.com/toefel18/go-deliver/DHLapp/build/default ./main &
